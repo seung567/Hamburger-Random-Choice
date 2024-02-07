baab2ce54745796fb12b68633d9b9c37edc5c8a1
seung567 <82252998+seung567@users.noreply.github.com>
2024-02-07 21:36:24 +0900

    20240207

---
+++ js/index.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+
+$(function() {
+
+	$('#slide_banner').bxSlider({
+
+		auto: true,
+		autoControls: true,
+		stopAutoOnClick: true,
+		pager: true,
+		slideWidth: 664,
+		minSlides: 3,
+		maxSlides: 3,
+		slideMargin: 10
+
+	});
+
+	$('#serchBtn').click(siteSearch);
+	$('.menubar span:eq(2)').click(shopmap);
+	$('.menubar span:eq(0)').click(randomsite);
+	$('#hambugerRandom').click(randomsite)
+
+	function siteSearch() {
+		var site = $('#siteChoice').val();
+		var searchDate = $('.searchInput').val();
+	
+		var siteValue;
+
+		if (site == '구글')  {
+			siteValue = 'https://www.google.com/search?q=' + searchDate;
+		} else if (site == '네이버') {
+			siteValue = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=' + searchDate;
+		} else if (site == '다음') {
+			siteValue = 'https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q=' + searchDate;
+		}
+
+		var winObj = window.open("", '검색', "width=1500px, height=1000px");
+		winObj.location.href = siteValue;
+	}
+	
+	function shopmap(){
+		var winObj = window.open("", '검색', "width=1500px, height=1000px");
+		winObj.location.href = './html/kakaomap.html';
+		
+	}
+	
+	function randomsite(){
+				window.location.href = './html/randomSite.html';
+	}
+});
